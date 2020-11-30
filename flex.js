c626eef36a1b5a8e0e9f8241e6c0666b391b9fd9
Flowdeeps <speedwolf@gmail.com>
2020-11-30 12:14:43 +0100

    F l e x  is now a little less agressive

--- flex.js
+++ flex.js
@@ -7,7 +7,7 @@ class flex extends HTMLElement {
     wrapper.setAttribute('class', 'flex');
     // styles
     const style = document.createElement("style");
-    style.textContent = ".flex {animation: flex ease-in-out 1s infinite} @keyframes flex {from {letter-spacing: normal} 50% {letter-spacing: 1em} to {letter-spacing: normal}}";
+    style.textContent = ".flex {animation: flex ease-in-out 1s infinite} @keyframes flex {from {letter-spacing: normal} 50% {letter-spacing: 0.1em} to {letter-spacing: normal}}";
     wrapper.textContent = this.textContent;
     this.shadowRoot.append(wrapper, style);
   }
